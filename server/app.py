83d26f4fceb3949641802ef1d6b730a631702daa
Ronnie Wanjala <ranjala90@gmail.com>
2024-10-05 13:08:19 +0300

    SQLAlchemy lab1

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -15,13 +15,45 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 
-@app.route('/')
-def index():
-    body = {'message': 'Flask SQLAlchemy Lab 1'}
-    return make_response(body, 200)
+@app.route('/earthquakes/<int:id>', methods=['GET'])
+def get_earthquake(id):
+    # Query the database for the earthquake with the specified id using filter_by and first
+    earthquake = Earthquake.query.filter_by(id=id).first()
 
-# Add views here
+    if earthquake:
+        # If found, return the earthquake data as JSON
+        return jsonify({
+            "id": earthquake.id,
+            "location": earthquake.location,
+            "magnitude": earthquake.magnitude,
+            "year": earthquake.year
+        }), 200
+    else:
+        # If not found, return a 404 status with an error message
+        return jsonify({"message": f"Earthquake {id} not found."}), 404
+    
+@app.route('/earthquakes/magnitude/<float:magnitude>', methods=['GET'])
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    
+    # Prepare the list of earthquake data
+    quakes = [
+        {
+            "id": quake.id,
+            "location": quake.location,
+            "magnitude": quake.magnitude,
+            "year": quake.year
+        }
+        for quake in earthquakes
+    ]
+    
+    # Return the count and the list of earthquakes as a JSON response
+    return jsonify({
+        "count": len(quakes),
+        "quakes": quakes
+    }), 200   
 
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
--- server/models.py
+++ server/models.py
@@ -2,8 +2,21 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
+
 metadata = MetaData()
 
 db = SQLAlchemy(metadata=metadata)
 
-# Add models here
+# models 
+
+class Earthquake(db.Model, SerializerMixin,):
+    __tablename__ = "earthquake"
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>"
+    
+    
